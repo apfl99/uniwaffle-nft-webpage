39a34160f12120bd80a9d4b3608360b33acbee47
LTSGOD <lts0424@naver.com>
2025-01-02 19:30:59 +0900

    feat: 2번째 교환 모션 추가

--- playground/src/modal/Modal.tsx
+++ playground/src/modal/Modal.tsx
@@ -1,4 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import Player from "lottie-react";
+import animationData from "../animation/light.json";
+import Card from "../modal/Card"; // Card 컴포넌트 경로
 import "./Modal.css";
 
 interface ModalProps {
@@ -7,16 +11,55 @@ interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null; // 모달이 열리지 않은 경우 렌더링하지 않음
-
-  const [leftPosition, setLeftPosition] = useState("0px");
+  const [isVisible, setIsVisible] = useState(true); // 애니메이션 상태
+  const [stage, setStage] = useState(1); // 애니메이션 단계 상태
+  const Firstcontrols = useAnimation();
+  const Secondcontrols = useAnimation();
 
   const handleExchangeClick = () => {
-    setLeftPosition("450px");
+    setIsVisible(false); // 사라지는 애니메이션 트리거
+    FirstCardAnimation();
+    SecondCardAnimation();
   };
 
+  const handleCloseTab = () => {
+    setIsVisible(true);
+    onClose();
+  };
+
+  const FirstCardAnimation = async () => {
+    // 첫 번째 애니메이션 실행
+    await Firstcontrols.start({
+      transform: "translateX(136.5%)",
+      transition: { duration: 2 },
+    });
+    // 두 번째 애니메이션 실행
+    await Firstcontrols.start({
+      opacity: 0,
+      transition: { duration: 0.5 },
+    });
+  };
+
+
+
+  const SecondCardAnimation = async () => {
+    // 첫 번째 애니메이션 실행
+    await Secondcontrols.start({
+      transform: "translateX(-147%)",
+      transition: { duration: 2 },
+    });
+    // 두 번째 애니메이션 실행
+    await Secondcontrols.start({
+      opacity: 0,
+      transition: { duration: 2 },
+    });
+  };
+
+
+  if (!isOpen) return null; // 모달이 열리지 않으면 렌더링하지 않음
+
   return (
-    <div
+    <motion.div
       style={{
         position: "fixed",
         top: 0,
@@ -29,11 +72,106 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
         justifyContent: "center",
         zIndex: 1000,
       }}
+      animate={{backgroundColor: isVisible ?  "fffff": "#313233"}}
     >
-      <div className="exchange-container">
+      
+      <motion.div className="exchange-container"
+        initial={{backgroundColor : "rgba(247, 250, 254, 1)"}}
+        animate={{backgroundColor : isVisible ? "#f7fafe" : "transparent"}}
+        transition={{ duration: 1 }}
+      >
         <div className="content-wrapper">
           <div className="card-container">
-            <div className="card" style={{left: leftPosition}}>
+            <motion.div className="card"
+              key={stage}
+              animate={Firstcontrols}
+              transition={{ duration: 2 }}
+            >
+              <div className="image-container">
+                <div className="image-wrapper">
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/de6380ad135d8dcbc54c9ac73b9dc8b6eeb05db1c14e516e5bf8f25283274a68?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8"
+                    className="image"
+                    alt="artwork preview"
+                  />
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/82f4316bb22998f33a8564a20eed1764eb429fd28d7d067059a1bc748945dd43?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8"
+                    className="profile-image"
+                    alt="User profile"
+                  />
+                </div>
+              </div>
+              <div className="info">
+                <div className="title-container">
+                  <div className="title">Uniwaffle Friends #192</div>
+                  <div className="description">
+                    A Collection of Uniwaffle 2025 Beta Test Commemoratives
+                  </div>
+                </div>
+              </div>
+              <div className="price-container">
+                <div className="price-text">1849 USHD</div>
+              </div>
+            </motion.div>
+            <motion.div className="price-large"
+              initial={{ opacity: 1 }} // 초기 상태
+              animate={{ opacity: isVisible ? 1 : 0 }} // 애니메이션 상태
+              transition={{ duration: 0.5 }}
+            >
+              1849 USHD
+            </motion.div>
+          </div>
+          
+          <motion.div className="exchange-info"
+            initial={{ opacity: 1 }} // 초기 상태
+            animate={{ opacity: isVisible ? 1 : 0 }} // 애니메이션 상태
+            transition={{ duration: 0.5 }}>
+            <div className="info-text">
+              <div className="main-text">
+                교환하려면
+                <br />
+                교환하기 버튼을 누르세요.
+              </div>
+              <div className="warning-text">
+                교환하면 이전으로 되돌릴 수 없습니다.
+              </div>
+            </div>
+            <div className="button-container">
+              <button className="exchange-button" onClick={handleExchangeClick}>
+                교환하기
+              </button>
+              <button className="close-button" onClick={onClose}>
+                창 닫기
+              </button>
+            </div>
+          </motion.div>
+
+
+
+          {/* 후광 이미지 */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: isVisible ? 0 : 1 }}
+            transition={{ duration: 0.5 }}
+            className="light-image2"
+          >
+            <Player
+              autoplay
+              loop
+              animationData={animationData}
+              className="light-image2"
+            />
+          </motion.div>
+
+          <motion.div className="new-card-wrapper"
+          initial={{opacity: 0}}
+          animate={{opacity: isVisible ? 0 : 1}}
+          transition={{ duration: 2, delay: 2 }}
+          >
+            {/* 새로 생성되는 카드 */}
+            <div className="card">
               <div className="image-container">
                 <div className="image-wrapper">
                   <img
@@ -62,48 +200,35 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
                 <div className="price-text">1849 USHD</div>
               </div>
             </div>
-            
-            <div className="price-large">1849 USHD</div>
-          </div>
+            <button 
+              className="close-button2" 
+              onClick={handleCloseTab}
+            >
+            닫기
+            </button>
+          </motion.div>
 
-
-          <div className="exchange-info">
-            <div className="info-text">
-              <div className="main-text">
-                 교환하려면
-                <br />
-                교환하기 버튼을 누르세요.
-              </div>
-              <div className="warning-text">
-                교환하면 이전으로 되돌릴 수 없습니다.
-              </div>
-            </div>
-            <div className="button-container">
-              <button
-                className="exchange-button"
-                onClick={handleExchangeClick}
-              >
-                교환하기
-              </button>
-              <button className="close-button" onClick={onClose}>
-                창 닫기
-              </button>
-            </div>
-          </div>
+          
 
           <div className="preview-container">
-              <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/89152492d7ab112b12c0a80950c134b3d88834f7b6f9d581c6bc32da0614bbd6?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8"
-                  className="preview-image"
-                  alt="preview"
-                />
-            <div className="preview-price">??? USHD</div>
+            <motion.img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/89152492d7ab112b12c0a80950c134b3d88834f7b6f9d581c6bc32da0614bbd6?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8"
+              className="preview-image"
+              alt="preview"
+              animate={Secondcontrols}
+            />
+            <motion.div className="preview-price" 
+              initial={{ opacity: 1 }} // 초기 상태
+              animate={{ opacity: isVisible ? 1 : 0 }} // 애니메이션 상태
+              transition={{ duration: 0.5 }}
+              >
+                ??? USHD
+              </motion.div>
           </div>
         </div>
-      </div>
-
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
