de9915c995dbc6b9764cc9ab9b3f57f4eb40b06a
LTSGOD <lts0424@naver.com>
2025-01-03 14:12:44 +0900

    refactor: NFT있을 때 화면 UI 수정

--- playground/src/effect/Card.tsx
+++ playground/src/effect/Card.tsx
@@ -3,7 +3,18 @@ import Player from "lottie-react";
 import scratchOff from "../animation/scratch-off.json";
 import coin from "../animation/coin.json";
 
-export const Card: React.FC = () => {
+interface CardProps {
+  nft: NFT;
+}
+
+interface NFT {
+	image: string;
+	name: string;
+	description: string;
+	mint_address: string;
+}
+
+export const Card: React.FC<CardProps> = ({nft}) => {
 
   return (
     <div className="nft-card-wrapper">
@@ -12,7 +23,7 @@ export const Card: React.FC = () => {
           <div className="nft-image-wrapper">
             <img
               loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/de6380ad135d8dcbc54c9ac73b9dc8b6eeb05db1c14e516e5bf8f25283274a68?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8"
+              src={nft.image}
               className="nft-artwork"
               alt="Uniwaffle Friends #192 NFT artwork"
             />
@@ -26,9 +37,9 @@ export const Card: React.FC = () => {
         </div>
         <div className="nft-content">
           <div className="nft-text-content">
-            <h2 className="nft-heading">Uniwaffle Friends #192</h2>
+            <h2 className="nft-heading">{nft.name}</h2>
             <p className="nft-description">
-              A Collection of Uniwaffle 2025 Beta Test Commemoratives
+              {nft.description}
             </p>
           </div>
         </div>
