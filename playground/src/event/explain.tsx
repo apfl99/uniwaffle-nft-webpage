9b4d9348a491bceab0a4ba3c9960f7c0d42dbc73
apfl99 <rmrehd604@gmail.com>
2025-01-08 18:29:18 +0900

    update explain wallet connect

--- playground/src/event/explain.tsx
+++ playground/src/event/explain.tsx
@@ -14,7 +14,7 @@ const Explain: React.FC = () => {
         <div id="step-wrapper">
           <div id="step-content">
             <div id="step-indicator">Step 1</div>
-            <div id="step-description">'지갑 연결하기' 버튼을 클릭해주세요.</div>
+            <div id="step-description">'Select Wallet' 버튼을 클릭해주세요.</div>
           </div>
         </div>
         <div id="arrow-separator">
@@ -35,16 +35,6 @@ const Explain: React.FC = () => {
         <div id="step-wrapper">
           <div id="step-content">
             <div id="step-indicator">Step 3</div>
-            <div id="step-description">'Trust Wallet'을 선택해주세요.</div>
-          </div>
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/4736354592a45aae614af75257d3c48d063b563bb4908c1347361a1b0d8d0e25?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8" id="trust-wallet-screenshot" alt="Trust Wallet 선택 화면 스크린샷" />
-        </div>
-        <div id="arrow-separator">
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a4db170f423df406e30685fb5556e4bdb116c1ce0348aa6e1f17a5035c7eef0b?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8" id="arrow-icon" alt="" />
-        </div>
-        <div id="step-wrapper">
-          <div id="step-content">
-            <div id="step-indicator">Step 4</div>
             <div id="step-description">유니와플 앱을 열어서 아래의 QR 코드를 인식하거나 링크를 복사하여 수동으로 코드를 입력하세요.</div>
             <div id="step-subtitle">[유니와플 앱 &gt;  크립토 메인 &gt; 바로가기 메뉴 'WalletConnect']</div>
           </div>
