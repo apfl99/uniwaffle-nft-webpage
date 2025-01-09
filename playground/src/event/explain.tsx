68b32dc74523f879b29ebeb933c8c90c930fe1bc
LTSGOD <lts0424@naver.com>
2025-01-09 14:19:41 +0900

    patch 삭제

--- playground/src/event/explain.tsx
+++ playground/src/event/explain.tsx
@@ -27,7 +27,7 @@ const Explain: React.FC = () => {
             <div id="step-indicator">Step 2</div>
             <div id="step-description">창이 표시되면 'Uniwaffle' 버튼을 클릭해주세요.</div>
           </div>
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/4caad8969094fd2c3a69dbd4aaef29f668a9091796cacc39425e1b3c232c01fc?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8" id="wallet-screenshot" alt="WalletConnect 버튼 위치 스크린샷" />
+          <img loading="lazy" src="https://cdn.discordapp.com/attachments/1326744308350718045/1326780591001047050/step2-img.png?ex=6780ac37&is=677f5ab7&hm=47cc8f1547e7e83608d1a4cd40a1482ac05cab8a9f127f50adbd47f541abcaea&" id="wallet-screenshot" alt="WalletConnect 버튼 위치 스크린샷" />
         </div>
         <div id="arrow-separator">
           <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a4db170f423df406e30685fb5556e4bdb116c1ce0348aa6e1f17a5035c7eef0b?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8" id="arrow-icon" alt="" />
@@ -38,7 +38,7 @@ const Explain: React.FC = () => {
             <div id="step-description">유니와플 앱을 열어서 아래의 QR 코드를 인식하거나 링크를 복사하여 수동으로 코드를 입력하세요.</div>
             <div id="step-subtitle">[유니와플 앱 &gt;  크립토 메인 &gt; 바로가기 메뉴 'WalletConnect']</div>
           </div>
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/d8d695fd5fc618389f160667ed846cdc886739bb047000afd7265d850f6890a7?placeholderIfAbsent=true&apiKey=5af3aa077a7b43c6a493f500437ba1d8" id="qr-screenshot" alt="WalletConnect QR 코드 스크린샷" />
+          <img loading="lazy" src="https://cdn.discordapp.com/attachments/1326744308350718045/1326780591378792500/step3-img.png?ex=6780ac37&is=677f5ab7&hm=94a0a1286f6ca1e0b514b4bc0a8f1bcd981890a81da909d0075d38bb3ab08f63&" id="qr-screenshot" alt="WalletConnect QR 코드 스크린샷" />
         </div>
       </div>
     </div>
